e6338594791c5913a73d2baed11a97cd4fb5d171
Ryan McGuire <ryanmcguire@Ryans-MacBook-Pro.local>
2023-12-31 17:56:15 -0600

    reorganzing and adding to research

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,23 @@
 export default function Home() {
   return (
     <main>
+      <h1>Research</h1>
       <div>
-        <h1>Research</h1>
         <h2>Retail Data Analytics</h2>
         <ul>
           <li>
             <a href="https://hbr.org/2023/02/why-retailers-fail-to-adopt-advanced-data-analytics"> 
               Why Retailers Fail to Adopt Advanced Data Analytics 
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h2>Web Apps</h2>
+        <ul>
+          <li>
+            <a href="https://www.reddit.com/r/webdev/comments/oo1znh/how_would_you_build_web_app_in_2021/?rdt=34567"> 
+              How Would You Build a Web App in 2021
             </a>
           </li>
         </ul>
